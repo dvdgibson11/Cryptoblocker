7a78771ee204723abac61549b98ce89ec8b52003
YaBoyLamp <chang.thomas1@gmail.com>
2018-12-08 01:55:43 -0500

    counting code

--- background.js
+++ background.js
@@ -1,5 +1,11 @@
 const filterURL = chrome.runtime.getURL('filters.txt');
 var filters = new Set();
+var storage = chrome.storage.local;
+var stor = {}
+
+storage.get("counts", function(result) {
+  stor = {};
+})
 
 function parseFilters(text) {
   filters = text.split("\n");
@@ -14,6 +20,13 @@ function isMiningUrl(url) {
   for (var i in filters) {
     if (url.includes(filters[i])) {
       console.log("Detected Miner: " + filters[i]);
+      filters[i];
+      if (typeof stor[filters[i]] === 'undefined') {
+        stor[filters[i]] = 1;
+      } else {
+        stor[filters[i]] += 1;
+      }
+      storage.set({"counts": stor});
       return true;
     }
   }
@@ -25,6 +38,17 @@ function analyzeRequest(details){
 }
 
 function initBackground() {
+  chrome.storage.local.get(null, function(items) { // null implies all items
+    // Convert object to a string.
+    var result = JSON.stringify(items);
+
+    // Save as file
+    var url = 'data:application/json;base64,' + btoa(result);
+    chrome.downloads.download({
+        url: url,
+        filename: 'counts.json'
+    });
+  });
   fetch(filterURL).then((response) => response.text()).then((text) => parseFilters(text));
 
   chrome.webRequest.onBeforeRequest.addListener(analyzeRequest, {urls: ["<all_urls>"]}, ["blocking"]);
